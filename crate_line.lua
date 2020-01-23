5bdef609e8c2e04bbfd3307e77736e562963b650
TheSink <lucianedean@gmail.com>
2020-01-22 16:30:33 -0800

    Update crate_line for new Actor API

--- crate_line.lua
+++ crate_line.lua
@@ -2,13 +2,15 @@
 
 Title: Crate Line Demo Script
 Author: TheSinkGod
-Updated: January 19 2020
+Updated: January 22 2020
 Description: This script is to be used on the Flat map, and creates a line of 10 generic crate models on the ground near the center.
 Intended Version: a0.0.3.0
 
 --]]
 
+local targetActor
+
 for i=0,1000,100 do
-  SpawnActorFromFile("metalcrate_64x96.obj")
-  MoveActor(0, i, 320)
+  targetActor = SpawnActorFromFile("metalcrate_64x96.obj")
+  MoveActor(targetActor, {0, i, 320})
 end
